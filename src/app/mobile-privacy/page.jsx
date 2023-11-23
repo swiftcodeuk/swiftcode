79a64c0ee952f2af0dc6ddc97cce8dfac6289728
coo0lzero <william.wargrave@swiftcode.uk>
2023-11-23 18:16:29 +0000

    Privacy policy for mobile

---
+++ src/app/mobile-privacy/page.jsx
@@ -0,0 +1,101 @@
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+function Content() {
+  return (
+    <div className="flex justify-center pt-0 pb-60 px-4 sm:px-8 lg:px-8">
+    <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+    <div className="mt-10 max-w-2xl space-y-6 text-base">
+            
+    <h1 class="text-2xl font-bold tracking-tight text-gray-900">Total Casino - Memory Challenge Privacy Policy</h1>
+  <p>Last revised: November 19, 2023</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Introduction</h2>
+  <p>Thank you for exploring our Application! This Privacy Policy ("Policy") outlines the terms "we," "us," and "our," referring to the Application Owner, and Total Casino - Memory Challenge, representing the online service provided by the Application Owner. Geared towards individuals interacting with the Application, including Visitors and Customers collectively known as "you" or "your," this Policy provides details on the collection, usage, disclosure, and processing of personal data during your engagement with the Application. Such interactions include visiting, signing up, using the Application, participating in events, or any other form of engagement. This Policy complements other notices or contractual terms provided to you. Your use of the Service implies your agreement to the collection and use of information as outlined in this Privacy Policy.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Contact Information</h2>
+  <p>If you have questions about the Policy or our privacy practices, please reach out to us using the details below:</p>
+  <p>Email: <a href="mailto:support@swiftcode.uk" class="text-blue-600">support@swiftcode.uk</a></p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Definitions</h2>
+  <p>Capitalized words in this Policy, whether singular or plural, carry the meanings described below:</p>
+  
+  <p><strong>Applicable Legislation:</strong> Legislation related to personal data protection, including the General Data Protection Regulation (GDPR) and other applicable laws.</p>
+  <p><strong>You:</strong> The individual or entity accessing or using the Service.</p>
+  <p><strong>Cookies:</strong> Small data pieces stored on your device when you visit the Application.</p>
+  <p><strong>Data Subject:</strong> A natural person whose Personal data is processed.</p>
+  <p><strong>Device:</strong> Any device accessing the Application, such as a computer, smartphone, or tablet.</p>
+  <p><strong>IP Address:</strong> Unique internet protocol address associated with your Internet activity.</p>
+  <p><strong>Personal Data:</strong> Information about an identified or identifiable natural person.</p>
+  <p><strong>Pixels:</strong> Software code integrated into the program code for tracking and analyzing User behavior.</p>
+  <p><strong>Technical Data:</strong> Information collected by the Application about your device and interaction.</p>
+  <p><strong>Visitor:</strong> A person visiting the Service.</p>
+  <p><strong>Service:</strong> Refers to Total Casino - Memory Challenge, the software program provided by the Application Owner.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Source of Personal Data</h2>
+  <p>We collect data directly from you through promotional forms and your communication via email, telephone, or postal address. While using our Service, we may request personally identifiable information, including but not limited to email address and usage data. Additionally, we automatically collect information when you use our app, such as IP address, app version, activities within the app, and usage date/time.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Processing of Personal Data</h2>
+  <p>As the controller of Personal data, we process it to:</p>
+  <ul>
+    <li>Provide and maintain our Service.</li>
+    <li>Contact you for updates or informative communications.</li>
+    <li>Manage your requests.</li>
+    <li>Evaluate business transfers.</li>
+    <li>Analyze data, identify trends, and improve our Service.</li>
+  </ul>
+  <p>We may share your personal information with Service Providers, in business transfers, with other users, or with your consent.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">RIGHT TO OPT-OUT (WITHDRAW CONSENT) AND OBJECT</h2>
+  <p>You can opt out of receiving communications from us. We prioritize protecting children's privacy and do not knowingly collect information from those under 18. You have the right to request deletion of your Personal Data.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Storage of Personal Data</h2>
+  <p>We store Personal data in databases on our premises and use data centers of relevant service providers. Security measures are in place to prevent unauthorized access.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Retention of Your Personal Data</h2>
+  <p>We retain Personal Data as long as necessary for the purposes stated in this Policy or as required by law.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Transfer of Personal Data to Third Parties</h2>
+  <p>We do not transfer Personal Data to third parties unless required by legislation, at your request, or to our Processors as specified in Article 8.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Transfer of Your Personal Data</h2>
+  <p>Your information, including Personal Data, may be processed outside your jurisdiction. We ensure such transfers comply with data protection standards.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Processors of Personal Data</h2>
+  <p>We may engage Processors to facilitate Personal data processing, ensuring they adhere to confidentiality obligations.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Disclosure of Your Personal Data</h2>
+  <p>In business transactions or law enforcement situations, we may disclose Personal Data.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Cross-Border Data Transfer</h2>
+  <p>Your data may be processed in countries with different data protection standards. We ensure adequate safeguards for such transfers.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Limitation of Liability Regarding Third-Party Resources</h2>
+  <p>Links to third-party resources are provided for informational purposes. We are not responsible for their policies and practices. Ensure you review their privacy policies before providing personal data.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Other Virtual Currency</h2>
+  <p>Our app includes a virtual currency system for in-app entertainment purposes only. This currency holds no real-world monetary value.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Policy Update</h2>
+  <p>We may update this Policy at our discretion. The updated version is effective upon accessibility. Changes will be notified through prominent notices or direct notifications. Review this Policy periodically for updates.</p>
+  
+  <h2 class="text-xl font-bold tracking-tight text-gray-900">Contact Us</h2>
+  <p>For questions about this Privacy Policy, contact us at:</p>
+  <p>Email: <a href="mailto:support@swiftcode.uk" class="text-blue-600">support@swiftcode.uk</a></p>
+           </div>
+  </div>
+  </div>
+  )
+}
+
+export default function Success() {
+    return (
+      <>
+        <Header />
+        <main>
+            <Content />
+        </main>
+        <Footer />
+      </>
+    )
+  }
